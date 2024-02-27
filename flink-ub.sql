2de60dcb511df4be3074213800cc01e61d9f1f46
jayjaywei <3516018306@qq.com>
2024-02-27 09:17:54 +0800

    new

--- flink-ub.sql
+++ flink-ub.sql
@@ -15,3 +15,36 @@ CREATE TABLE user_behavior (
     'connector.properties.bootstrap.servers' = '172.16.122.17:9092',
     'format.type' = 'json'
 );
+CREATE TABLE buy_cnt_per_hour (
+    hour_of_day BIGINT,
+    buy_cnt BIGINT
+) WITH (
+    'connector.type' = 'elasticsearch',
+    'connector.version' = '7',
+    'connector.hosts' = 'http://172.16.122.13:9200',
+    'connector.index' = 'buy_cnt_per_hour',
+    'connector.document-type' = 'user_behavior',
+    'connector.bulk-flush.max-actions' = '1',
+    'update-mode' = 'append',
+    'format.type' = 'json'
+);
+
+INSERT INTO buy_cnt_per_hour
+SELECT HOUR(TUMBLE_START(ts, INTERVAL '1' HOUR)), COUNT(*)
+FROM user_behavior
+WHERE behavior = 'buy'
+GROUP BY TUMBLE(ts, INTERVAL '1' HOUR);
+
+--存放每10分钟累计独立用户数
+CREATE TABLE cumulative_uv (
+    time_str STRING,
+    uv BIGINT
+) WITH (
+    'connector.type' = 'elasticsearch',
+    'connector.version' = '7',
+    'connector.hosts' = 'http://172.16.122.13:9200',
+    'connector.index' = 'cumulative_uv',
+    'connector.document-type' = 'user_behavior',
+    'update-mode' = 'upsert',
+    'format.type' = 'json'
+);
